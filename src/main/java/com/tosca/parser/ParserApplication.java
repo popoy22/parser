bef8e68dba8b06707f53fa5df2006d011654c069
Poy Refuerzo <popoy@yhmwhy.com>
2020-09-11 12:56:38 +0800

    no message

--- src/main/java/com/tosca/parser/ParserApplication.java
+++ src/main/java/com/tosca/parser/ParserApplication.java
@@ -1,13 +1,20 @@
 package com.tosca.parser;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParserApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getMapper() {
+		return  new ModelMapper();
 	}
 
 }
--- src/main/java/com/tosca/parser/serviceImpl/ParserServiceImpl.java
+++ src/main/java/com/tosca/parser/serviceImpl/ParserServiceImpl.java
@@ -2,7 +2,14 @@ package com.tosca.parser.serviceImpl;
 
 import com.tosca.parser.dto.response.TestDTO;
 import com.tosca.parser.dto.response.TestResultDTO;
+import com.tosca.parser.dto.response.testsuite.TestCaseDTO;
+import com.tosca.parser.entity.TestCase;
+import com.tosca.parser.entity.TestSuite;
+import com.tosca.parser.repository.TestSuiteRepository;
 import com.tosca.parser.service.ParserService;
+import com.tosca.parser.service.TestSuiteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -20,9 +27,19 @@ import java.util.List;
 @Service
 public class ParserServiceImpl implements ParserService {
 
+    @Autowired
+    TestSuiteRepository testSuiteRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+
     public List<TestDTO> parse(String url) throws ParserConfigurationException, IOException, SAXException {
 
+
+        TestSuite testSuite = new TestSuite();
         List<TestDTO> testDTOList = new ArrayList<>();
+
 
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -40,7 +57,10 @@ public class ParserServiceImpl implements ParserService {
 
 
         for (int i = 0; i < nodeList.getLength(); i++) {
+            testSuite = new TestSuite();
             Node node = nodeList.item(i);
+
+
 
             if(node.getNodeType() == Node.ELEMENT_NODE) {
                 Element elem = (Element) node;
@@ -48,21 +68,41 @@ public class ParserServiceImpl implements ParserService {
                 testDTO.setName( elem.getAttribute("name"));
                 List<TestResultDTO> resultDTOList = new ArrayList<>();
 
+                // Adding in Database
+                testSuite.setName(elem.getAttribute("name"));
+
+
+
                 for(int r = 0; r < elem.getElementsByTagName("testcase").getLength(); r++){
+
+
+
 
                     TestResultDTO testResultDTO = new TestResultDTO();
                     Node node1 = elem.getElementsByTagName("testcase").item(r);
                     Element elem1 = (Element) node1;
-
                     testResultDTO.setName(elem1.getAttribute("name"));
                     testResultDTO.setResult(elem1.getChildNodes().item(1).getNodeName());
                     resultDTOList.add(testResultDTO);
+
+
+
+                    // Adding in Database Repository
+                    TestCase testCase = new TestCase();
+                    testCase.setName(elem1.getAttribute("name"));
+                    testCase.setResult(elem1.getChildNodes().item(1).getNodeName());
+                    testSuite.getTestCases().add(testCase);
+
 
                 }
                 testDTO.setResults(resultDTOList);
                 testDTOList.add(testDTO);
             }
 
+            testSuiteRepository.save(testSuite);
+
+
+            //testSuiteRepository.save(testSuite);
 
         }
 
